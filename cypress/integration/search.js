dd911df699bf1fbeff06b38efd838a5bbbc56eb3
MartinQAGL <martin.zasso@globallogic.com>
2022-01-14 14:22:34 -0300

    Se agrega primeros elementos de busqueda

--- cypress/integration/search.js
+++ cypress/integration/search.js
@@ -1,5 +1,8 @@
-describe('Buscar elementos', ()=>{
-    it('Busqueda de elementos con multiples resultados', ()=>{
-    cy.visit('/');      
-    }) 
+describe('Buscar elementos', () => {
+    it('Busqueda de elementos con multiples resultados', () => {
+        cy.visit('/');
+        cy.fixture('index').then((index) => {
+            cy.get(index.searchButton)
+        })
+    })
 })
