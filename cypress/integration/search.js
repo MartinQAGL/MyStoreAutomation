43906b90e97dded978b599754126f68c1dd785ad
MartinQAGL <martin.zasso@globallogic.com>
2022-01-14 15:22:42 -0300

    Primeros customs commands

--- cypress/integration/search.js
+++ cypress/integration/search.js
@@ -5,19 +5,13 @@ describe('Buscar elementos', () => {
         cy.visit('/')
     })
     it('Busqueda de elementos con multiples resultados', () => {
-        cy.fixture('index').then((index) => {
-            cy.get(index.searchBox).type('dress')
-            cy.get(index.searchButton).click()
-        })
+        cy.search('dress')
         cy.fixture('searchResult').then((searchResult) => {
             cy.get(searchResult.title).should('contain', 'dress')
         })
     })
     it('Busqueda de elementos sin resultados', () => {
-        cy.fixture('index').then((index) => {
-            cy.get(index.searchBox).type('qwerty')
-            cy.get(index.searchButton).click()
-        })
+        cy.search('qwerty')
         cy.fixture('searchResult').then((searchResult) => {
             cy.get(searchResult.alert).should('contain', 'No results were found for your search')
         })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -24,3 +24,9 @@
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 /// <reference types="Cypress" />
+Cypress.Commands.add('search', (value) => {
+    cy.fixture('index').then((index) => {
+        cy.get(index.searchBox).type(value)
+        cy.get(index.searchButton).click()
+    })
+})
