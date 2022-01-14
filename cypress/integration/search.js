9b8e662308469a7020c293e2d8f30f7023a756e0
MartinQAGL <martin.zasso@globallogic.com>
2022-01-14 14:42:07 -0300

    Se finaliza primera prueba

--- cypress/integration/search.js
+++ cypress/integration/search.js
@@ -2,7 +2,11 @@ describe('Buscar elementos', () => {
     it('Busqueda de elementos con multiples resultados', () => {
         cy.visit('/');
         cy.fixture('index').then((index) => {
-            cy.get(index.searchButton)
+            cy.get(index.searchBox).type('dress')
+            cy.get(index.searchButton).click()
+        })
+        cy.fixture('searchResult').then((searchResult)=> {
+            cy.get(searchResult.title).should('contain','dress')
         })
     })
 })
