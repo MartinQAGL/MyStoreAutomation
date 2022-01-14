26c40213d2e54804bc48886f4d33a7328a652264
MartinQAGL <martin.zasso@globallogic.com>
2022-01-14 15:00:56 -0300

    Se finaliza segundo prueba

--- cypress/integration/search.js
+++ cypress/integration/search.js
@@ -1,12 +1,25 @@
+
+
 describe('Buscar elementos', () => {
+    beforeEach(() => {
+        cy.visit('/')
+    })
     it('Busqueda de elementos con multiples resultados', () => {
-        cy.visit('/');
         cy.fixture('index').then((index) => {
             cy.get(index.searchBox).type('dress')
             cy.get(index.searchButton).click()
         })
-        cy.fixture('searchResult').then((searchResult)=> {
-            cy.get(searchResult.title).should('contain','dress')
+        cy.fixture('searchResult').then((searchResult) => {
+            cy.get(searchResult.title).should('contain', 'dress')
+        })
+    })
+    it('Busqueda de elementos sin resultados', () => {
+        cy.fixture('index').then((index) => {
+            cy.get(index.searchBox).type('qwerty')
+            cy.get(index.searchButton).click()
+        })
+        cy.fixture('searchResult').then((searchResult) => {
+            cy.get(searchResult.alert).should('contain', 'No results were found for your search')
         })
     })
 })
