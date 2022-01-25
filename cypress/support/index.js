ec8772bce762fc3158007ebada8eb88dd0b1957a
MartinQAGL <martin.zasso@globallogic.com>
2022-01-25 14:44:32 -0300

    Se escribe caso de prueba registro

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -17,6 +17,7 @@
 import './commands'
 import './loginCommands.js'
 import './busquedaCommands.js'
+import './registroCommands.js'
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
